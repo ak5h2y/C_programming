943aa3fa472d34102cf637a1bcd14f314141fa52
ak5h2y <44160604+ak5h2y@users.noreply.github.com>
2020-04-12 23:05:08 +0530

    to have double quotes in print

--- hello_world.c
+++ hello_world.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
 int main(void){
-    printf("Hello world!\nLets learn C programming!"); //reduce the number of printf statements
+    printf("Hello world!\nLets learn \"C\" programming!"); //enables us to have double quote in print
     return 0;
 }
