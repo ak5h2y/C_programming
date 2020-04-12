60724776fc0a57edc21f844e897715493c4ec02a
ak5h2y <44160604+ak5h2y@users.noreply.github.com>
2020-04-12 23:54:01 +0530

    comments added
    comments to understand the structure of a c program
    no changes into source code

--- for_loop.c
+++ for_loop.c
@@ -1,8 +1,13 @@
+// preprocessor directive
 #include <stdio.h>
+// main function
 int main(void) {
+    // variable declarations
     int count = 0;
+    // executable statements
     for(count=1; count<6; count++){
         printf("%d \n",count); //print numbers 1 to 5
     }
+    // return statement
     return 0;
 }
