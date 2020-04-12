ccd5d1ebc6a5faeee84bdc1c9597f79def76035e
ak5h2y <44160604+ak5h2y@users.noreply.github.com>
2020-04-12 23:01:08 +0530

    single printf for multiple print statements

--- hello_world.c
+++ hello_world.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 int main(void){
-    printf("Hello world!\n"); //to have a new line
-    printf("Lets learn C programming!");
+    printf("Hello world!\nLets learn C programming!"); //reduce the number of printf statements
     return 0;
 }
