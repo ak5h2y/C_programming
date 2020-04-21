85c25e73a17e6824eb04a04cf234436525ef82c8
ak5h2y <44160604+ak5h2y@users.noreply.github.com>
2020-04-21 15:32:13 +0530

    Use variables in loops

--- using_variables.c
+++ using_variables.c
@@ -1,28 +1,12 @@
 #include <stdio.h>
-int main() {
-    /* Variable names can use:  
-    lowercase and uppercase letters (characters) and digits
-    do not use special characters like @ # & " ...
-    do not use accented characters like é è à ...
-    do not start with a digit
-    start only with a letter
-    spaces are forbidden
-     _ may be used instead of a space in the name of the variable
-    YouCanUseUppercaseLettersBetweenWordsInsteadOfSpaces   */
-    
-    //Declaration
-    int balance;//creating a variable containing integer values
-    //Definition
-    balance = 50;//assigning the value 50 into the balance variable
-    printf("I have %d dollars in my account\n",balance);
-    //expense of 40 dollars
-    balance = balance - 40;
-    printf("I have %d dollars in my account\n",balance);
-    //add 20 dollars in my account
-    balance = balance + 20;
-    printf("I have %d dollars in my account\n",balance);
-    //expense of 30 dollars
-    balance = balance - 30;
-    printf("I have %d dollar in my account\n",balance);
+int main(void) {
+    int i;
+    int numberOfHazelnuts = 0;
+    int distanceTraveled = 0;
+    for(i = 0; i < 9 ; i++) {
+        printf("At %d miles I have %d hazelnuts.\n", distanceTraveled, numberOfHazelnuts);
+        distanceTraveled = distanceTraveled + 1;
+        numberOfHazelnuts = numberOfHazelnuts + 3;
+    }
     return 0;
 }
