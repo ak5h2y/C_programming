c3d75e45e9dae8fdef9216d3bdfc72a322b65037
ak5h2y <44160604+ak5h2y@users.noreply.github.com>
2020-04-21 15:41:48 +0530

    Read integer user input using scanf()

--- compute_with_integers.c
+++ compute_with_integers.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 int main() {
-    printf("3+2 equals %d and 3-2 equals %d and 3*2 equals %d\n", 3+2, 3-2, 3*2);
-    printf("3+2*3 equals %d and (3+2)*3 equals %d\n", 3+2*3, (3+2)*3);
-    printf("2*8-2*7-4 equals %d\n", 2*8-2*7-4);
-    printf("2*(8-2*(7-4)) equals %d\n", 2*(8-2*(7-4)));
-    printf("2*(8-2*7)-4 equals %d\n", 2*(8-2*7)-4);
+    int age;//DECLARE
+    printf("What is your age ?\n"); 
+    scanf("%d",&age); //GET user input
+    printf("You are %d years old\n", age);//USE
     return 0;
 }
