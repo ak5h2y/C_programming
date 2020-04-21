de22b31760662cc60bb69c688d7da5f694802b25
ak5h2y <44160604+ak5h2y@users.noreply.github.com>
2020-04-21 15:55:42 +0530

    read multiple user inputs using scanf() in a loop

--- compute_with_integers.c
+++ compute_with_integers.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
-int main(void) {
-    int first, second, third;
-    printf("Please enter three integers, separated by commas: ");
-    scanf("%d,%d,%d", &first, &second, &third);
-    printf("You entered: %d for first, %d for second, %d for third.\n", first, second, third);
-    
-    return 0;
+int main() {
+    int howMany = 0;
+    int sum = 0;
+    int numberRead = 0;
+    printf("How many items do you want to add?\n");
+    scanf("%d",&howMany);
+    for(int i = 0; i < howMany; i++){
+        scanf("%d",&numberRead);
+        printf("I have read %d from the input terminal\n",numberRead);
+        sum = sum + numberRead;
+        printf("sum equals %d\n",sum);
+    }
+  return 0;
 }
