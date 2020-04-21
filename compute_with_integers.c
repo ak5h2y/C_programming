db28d353436c296b6281b7e55434f1a266157404
ak5h2y <44160604+ak5h2y@users.noreply.github.com>
2020-04-21 14:50:31 +0530

    Perform simple integer arithmetic (+, -, *, ())

--- compute_with_integers.c
+++ compute_with_integers.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-int main(void){
-    printf("If I have %d bills worth %d dollars each then I have %d dollars.",3,5,3*5);
+int main() {
+    printf("3+2 equals %d and 3-2 equals %d and 3*2 equals %d\n", 3+2, 3-2, 3*2);
+    printf("3+2*3 equals %d and (3+2)*3 equals %d\n", 3+2*3, (3+2)*3);
+    printf("2*8-2*7-4 equals %d\n", 2*8-2*7-4);
+    printf("2*(8-2*(7-4)) equals %d\n", 2*(8-2*(7-4)));
+    printf("2*(8-2*7)-4 equals %d\n", 2*(8-2*7)-4);
     return 0;
 }
