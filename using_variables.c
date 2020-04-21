e284f4aa2d7492815bb7097cb6380b8d9f7e19c3
ak5h2y <44160604+ak5h2y@users.noreply.github.com>
2020-04-21 15:17:52 +0530

    do's and don'ts in Naming your variables

--- using_variables.c
+++ using_variables.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 int main() {
+    /* Variable names can use:  
+    lowercase and uppercase letters (characters) and digits
+    do not use special characters like @ # & " ...
+    do not use accented characters like é è à ...
+    do not start with a digit
+    start only with a letter
+    spaces are forbidden
+     _ may be used instead of a space in the name of the variable
+    YouCanUseUppercaseLettersBetweenWordsInsteadOfSpaces   */
+    
+    //Declaration
     int balance;//creating a variable containing integer values
+    //Definition
     balance = 50;//assigning the value 50 into the balance variable
     printf("I have %d dollars in my account\n",balance);
     //expense of 40 dollars
